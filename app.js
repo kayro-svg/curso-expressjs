9de1345d591aab2da4bdb1e1dfc8640cea05cf06
Kayro Valero <sannelvalero@gmail.com>
2025-05-05 11:36:48 -0500

    chore: add body-parser dependency and update license type in package.json and package-lock.json

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +16,25 @@ app.get('/', (req, res) => {
         `);
 }
 );
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`
+        <h1> Usuario con ID: ${userId}</h1
+        `);
+}
+);
+
+app.get('/search', (req, res) => {
+    const terms = req.query.termino || 'No es especificado';
+    const category = req.query.categoria || 'Todas';
+
+    res.send(`
+        <h1> Busqueda de productos</h1>
+        <p>Termino: ${terms}</p>
+        <p>Categoria: ${category}</p>
+        `);
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}, aplicacion funcionando`);
