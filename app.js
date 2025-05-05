3321d0bc3ceee085f190d3b14d94ffc0e90f7ced
Kayro Valero <sannelvalero@gmail.com>
2025-05-05 10:36:20 -0500

    chore: initialize project with package.json and package-lock.json for express.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1> Curso Express.js</h1>
+        <p>Esto es un aplicacion node.js con express.js</p>
+        <p>Corre en el puerto:${PORT}</p>
+        `);
+}
+);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}, aplicacion funconando`);
+}
+);
